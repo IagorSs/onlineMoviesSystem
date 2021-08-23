042f2dbde267f65a4f87a3a5b684008262414907
IagorSs <iagor.penaranda@uol.com.br>
2021-08-23 07:51:00 -0300

    Update to use Cors

--- src/config/express/index.js
+++ src/config/express/index.js
@@ -3,8 +3,11 @@ const app = express();
 const routes = require("../../routes");
 const { port } = require("../env");
 
+const cors = require('cors');
+
 app.set("port", port || 3000);
 
 app.use(express.json());
+app.use(cors());
 Object.keys(routes).forEach((key) => app.use(`/api/v1/${key}`, routes[key]));
 module.exports = app;
